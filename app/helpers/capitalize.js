41560194ff1cd1116dcdeac92dc150310a3cc37a
markosandmarkos <martin.nazaryan@outlook.com>
2018-08-17 18:58:20 +0400

    helpers

---
+++ app/helpers/capitalize.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export function capitalize(params/*, hash*/) {
+  return params;
+}
+
+export default helper(capitalize);
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import wait from 'rarwe/utils/wait';
 
 export default Route.extend({
 
